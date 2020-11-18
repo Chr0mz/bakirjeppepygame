99e6edf78f0b87309551861bf2ff9ebba0b5eab6
QuaRench <jeppe@thodsen.net>
2020-11-18 09:05:48 +0100

    Lavede Archer Tower og PyroTower classes

---
+++ ArcherTower.py
@@ -0,0 +1,10 @@
+import pygame
+
+class ArcherTower:
+    def __init__(self):
+        skin = pygame.image.load('./assets/Archer Tower.png')
+        
+
+    def shoot(self):
+        pass
+    
---
+++ PyroTower.py
@@ -0,0 +1,10 @@
+import pygame
+
+class PyroTower:
+    def __init__(self):
+        skin = pygame.image.load('./assets/Pyro Tower.png')
+        
+
+    def shoot(self):
+        pass
+    
