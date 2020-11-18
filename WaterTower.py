94e7c4a76ae2f47aeb852fe89c109a0a997abb12
QuaRench <jeppe@thodsen.net>
2020-11-18 09:36:10 +0100

    tilføjede shootingspeed

--- WaterTower.py
+++ WaterTower.py
@@ -1,9 +1,11 @@
 import pygame
 
 class WaterTower:
-    def __init__(self):
+    def __init__(self, shootingSpeed):
         skin = pygame.image.load('./assets/Water Tower.png')
+        shootingspeed = shootingSpeed
         
+
 
     def shoot(self):
         pass
